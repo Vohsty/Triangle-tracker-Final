2aa3138e4b5fbb3de785360fd6e96f769ed30668
Vohsty <stevekimanthi87@gmail.com>
2019-03-24 17:18:40 +0300

    add equilateral.html

---
+++ js/scrips.js
@@ -0,0 +1,13 @@
+
+function create(a,b,c) {
+  if (a+b>c && c+b>a && a+c>b && a==b && b==c)
+  alert ("Equilateral triangle")
+  else if (a+b>c && c+b>a && a+c>b && a==b || b==c || a==c)
+    alert("isoscelese")
+  else if (a+b>c && c+b>a && a+c>b && a!=b!=c)
+    alert("scalene")
+    else
+    alert("Wacha ujinga")
+    }
+
+  
